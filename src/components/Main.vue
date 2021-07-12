de873df1c7193739710d706269a9795f10994436
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-12 12:07:43 +0200

    base milestone 1

---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <Search @searchN="a"/>
+</template>
+
+<script>
+// import axios from 'axios'
+import Search from "@/components/Search";
+export default {
+    name: 'Main',
+    components: {
+        Search,
+    },
+
+    data(){
+        return{
+            searchValue:'',
+        }
+    },
+
+    methods: {
+     a(Input){
+            this.searchValue =Input;
+            console.log(this.searchValue);
+        }
+  }
+}
+
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <form>
+      <div>
+          <input 
+          type="text" 
+          placeholder="Cerca"
+          v-model.trim="inputText"
+          >
+      </div>
+      <div>
+          <button 
+          type="submit" 
+          @click.prevent="$emit('searchN',inputText)"
+          >Search</button>
+      </div>
+  </form>
+  
+</template>
+
+<script>
+export default {
+    name:'Search',
+    data() {
+        return {
+            inputText:''
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
