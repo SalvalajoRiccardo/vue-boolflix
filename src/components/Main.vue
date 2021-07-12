7e5db2465e4e29cc01178796a04a85d67a165ab2
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-12 12:38:44 +0200

    errore chiamata api

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 import Search from "@/components/Search";
 export default {
     name: 'Main',
@@ -14,14 +14,41 @@ export default {
     data(){
         return{
             searchValue:'',
+            apiBase: 'https://api.themoviedb.org/3/',
+            apiKey: '2467b66cc2e2dd66d129967969509c91'
         }
     },
 
+    created() {
+        this.getFilms()
+    },
+
     methods: {
-     a(Input){
+        a(Input){
             this.searchValue =Input;
             console.log(this.searchValue);
-        }
+        },
+
+        getFilms(){
+            axios
+                .get(this.apiBase+'search/movie?', {
+                    params:{
+                        api_key: this.apiKey,
+                        query: this.searchValue,
+                        lenguage: 'it-IT'
+                    }
+                    
+                })
+
+                .then(resp => {
+                    console.log(resp);
+                    
+                })
+
+                .catch(error => {
+                    console.log('Errore: ', error);
+                });
+        },
   }
 }
 
