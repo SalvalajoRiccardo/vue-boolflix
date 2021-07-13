b708e844dba86501f47681ef59e64185e64286e7
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-13 16:30:37 +0200

    bonus - milestone 3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="card">
+        <img v-if="det.poster_path" :src="'https://image.tmdb.org/t/p/w342/'+det.poster_path" :alt="det.title">
         <h3>{{det.title}}</h3>
         <span>{{det.original_title}}</span>
         <p>Language:  
           <img v-if="flagsArray.includes(det.original_language)" class="flag" :src="require('../assets/img/'+det.original_language+'.png')" :alt="det.original_language">
           <span v-else>{{det.original_language}}</span>
         </p> 
-        <span>Vote: {{det.vote_average}}</span>
+        <span>Vote: <i v-for="(star,index) in stars(det.vote_average)" :key="index"  class="fas fa-star yellow-star"></i> </span>
   </div>
 </template>
 
 <script>
 export default {
- name: 'Card',
- props: ['det'],
- data(){
-     return{
+    name: 'Card',
+    props: ['det'],
+    data(){
+        return{
          flagsArray: ['en','fr','it']
-     }
- }
+
+        }
+    },
+    methods: {
+        stars(vote){
+         return parseInt(Math.round(vote / 2))
+        }  
+    }
 }
 </script>
 
@@ -26,6 +33,9 @@ export default {
     .card{
         .flag {
         width: 20px;
+        }
+        .yellow-star {
+            color: gold;
         }
         
     }   
--- src/components/CardTv.vue
+++ src/components/CardTv.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="card">
+    <img v-if="detTv.poster_path" :src="'https://image.tmdb.org/t/p/w342/'+detTv.poster_path" :alt="detTv.name">
         <h3>{{detTv.name}}</h3>
         <span>{{detTv.original_name}}</span>
         <p>Language:  
           <img v-if="flagsArray.includes(detTv.original_language)" class="flag" :src="require('../assets/img/'+detTv.original_language+'.png')" :alt="detTv.original_language">
           <span v-else>{{detTv.original_language}}</span>
         </p> 
-        <span>Vote: {{detTv.vote_average}}</span>
+        <span>Vote: <i v-for="(star,index) in stars(detTv.vote_average)" :key="index"  class="fas fa-star yellow-star"></i> </span>
   </div>
 </template>
 
 <script>
 export default {
- name: 'CardTv',
- props: ['detTv'],
- data(){
-     return{
-         flagsArray: ['en','fr','it']
-     }
- }
+  name: 'CardTv',
+  props: ['detTv'],
+  data(){
+    return{
+      flagsArray: ['en','fr','it']
+    }
+  },
+  methods: {
+        stars(vote){
+         return parseInt(Math.round(vote / 2))
+        }  
+    }
+ 
 }
 </script>
 
@@ -27,6 +34,10 @@ export default {
         .flag {
         width: 20px;
         }
+        .yellow-star {
+            color: gold;
+        }
+        
         
     }   
 </style>
