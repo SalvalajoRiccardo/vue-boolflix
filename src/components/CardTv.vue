96624d6a4a5367837222ee14aa14b23e9978bb41
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-13 14:06:18 +0200

    fine milestone 2

---
+++ src/components/CardTv.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+        <h3>{{detTv.name}}</h3>
+        <span>{{detTv.original_name}}</span>
+        <p>Language:  
+          <img v-if="flagsArray.includes(detTv.original_language)" class="flag" :src="require('../assets/img/'+detTv.original_language+'.png')" :alt="detTv.original_language">
+          <span v-else>{{detTv.original_language}}</span>
+        </p> 
+        <span>Vote: {{detTv.vote_average}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'CardTv',
+ props: ['detTv'],
+ data(){
+     return{
+         flagsArray: ['en','fr','it']
+     }
+ }
+}
+</script>
+
+<style scoped lang='scss'>
+    .card{
+        .flag {
+        width: 20px;
+        }
+        
+    }   
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,6 +7,7 @@
   </div>
   <div>
       <h2>TV Series</h2>
+      <CardTv v-for="(item, index) in tvResults" :key="index" :detTv="item"/>
   </div>
  
 </div>
@@ -16,17 +17,20 @@
 import axios from 'axios'
 import Search from "@/components/Search";
 import Card from "@/components/Card";
+import CardTv from "@/components/CardTv";
 export default {
     name: 'Main',
     components: {
         Search,
         Card,
+        CardTv,
     },
 
     data(){
         return{
             searchValue:'',
             filmResults:'',
+            tvResults:'',
             apiBase: 'https://api.themoviedb.org/3/',
             apiKey: '2467b66cc2e2dd66d129967969509c91'
         }
@@ -38,6 +42,7 @@ export default {
             if ( searchValue) {
                 this.searchValue = searchValue;
                 this.filmResults = '';
+                this.tvResults='';
                 axios
                     .get(this.apiBase+'search/movie', {
                         params:{
@@ -50,17 +55,37 @@ export default {
     
                     .then(res => {
                         this.filmResults = res.data.results;
-                        console.log(this.filmResults);
+                        
                         
                     })
     
                     .catch(error => {
                         console.log('Errore: ', error);
                     });
-                  
+                
+                axios
+                    .get(this.apiBase+'search/tv', {
+                        params:{
+                            api_key: this.apiKey,
+                            query: this.searchValue,
+                            language: 'it-IT'
+                        }
+                        
+                    })
+    
+                    .then(tav => {
+                        this.tvResults = tav.data.results;
+                        console.log(this.tvResults);
+                        
+                    })
+    
+                    .catch(error => {
+                        console.log('Errore: ', error);
+                    });
             } 
         },
-  }
+
+    }
 }
 
 
